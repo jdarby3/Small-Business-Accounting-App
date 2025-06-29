8d3b5e54d1801c5e61a771768f6b9f57d0dfa02b
James <86382991+jdarby3@users.noreply.github.com>
2025-06-29 13:16:02 +0000

    still working

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
+from datetime import date
+
+accounts = ()
+
+transactions = ()
+
 class Account:
-  def __init__(self, id, name, atype, balance):
-    self.id = id
-    self.type = atype
-    self.name = name
-    self.balance = balance
+  def __init__(self, acc_name, acc_id, acc_type, acc_balance):
+    self.acc_name = acc_name
+    self.acc_id = acc_id
+    self.acc_type = acc_type
+    self.acc_balance = acc_balance
+    self.transactions = ()
 
 class Transaction:
-  def __init__(self, id, account, creditdebit, amount, description):
-    self.id = id
-    self.creditdebit = creditdebit
-    self.account = account
-    self.amount = amount
-    #f 1000 <= int(self.account) <= 3999
-      #write so credit an debit gets properly added and subtracted from proper accounts
+  def __init__(self, trans_date, trans_num, acc_id, debit, credit, trans_desc):
+    self.trans_date = date.today()
+    self.trans_num = len(transactions) + 1
+    self.acc_id = acc_id
+    self.trans_acc_num = trans_acc_num
+    self.debit = debit
+    self.credit = credit
+    self.trans_desc = trans_desc
 
-def New_Account():
-  #function that will be called when new account button pressed
-  pass
+def add_transaction(trans, acc):
+  if trans.acc_id() == acc.acc_id()
+    
 
-def New_Transaction(Account, id, account, creditdebit, amount, description):
-  #make this function create a new transaction object
-  if Account.id == Transaction.account:
-    Account.balance += Transaction.amount
-    print (f'{Account.name} balance is now {Account.balance}')
-  else:
-    print ('Acount ID does not match')
 
-BluevineChecking = Account('1010', 'BlueVine Checking', 'Asset', 0)
 
-ToolPurchase = Transaction('1234', '1010', 'credit', 90)
 
-New_Transaction(BluevineChecking, ToolPurchase)
+
