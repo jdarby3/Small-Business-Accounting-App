e8637ff7fc7f65fe177cb95191f9643c72561278
James <86382991+jdarby3@users.noreply.github.com>
2025-06-29 00:13:52 +0000

    hi

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 class Account:
-  def __init__(self, id, type, balance):
+  def __init__(self, id, name, type, balance):
     self.id = id
     self.type = type
     self.balance = balance
 
   def transaction(self, creditdebit, amount):
+    ##creates a transaction in the account
+    if creditdebit.upper() == 'CREDIT'
+
     self.balance += amount
     return self.balance
 
-#class Transaction
- # def __init__(self, account_id, creditdebit, amount)
-  #  self.account_id = account_id
-   # self.creditdebit = creditdebit
-    #self.amount = amount
-
-  #def 
-
-BluevineChecking = Account('1010', 'Asset', 0)
+BluevineChecking = Account('1010', 'BlueVine Checking', 'Asset', 0)
 
 print(BluevineChecking.transaction('debit', 25))
