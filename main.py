4cae261d1d783273da7cd9ca41674b70a912cd42
James <86382991+jdarby3@users.noreply.github.com>
2025-06-29 01:19:24 +0000

    still young

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 class Account:
-  def __init__(self, id, name, type, balance):
+  def __init__(self, id, name, atype, balance):
     self.id = id
-    self.type = type
+    self.type = atype
+    self.name = name
     self.balance = balance
 
-  def transaction(self, creditdebit, amount):
-    ##creates a transaction in the account
-    if creditdebit.upper() == 'CREDIT'
+class Transaction:
+  def __init__(self, id, account, creditdebit, amount, description):
+    self.id = id
+    self.creditdebit = creditdebit
+    self.account = account
+    self.amount = amount
+    #f 1000 <= int(self.account) <= 3999
+      #write so credit an debit gets properly added and subtracted from proper accounts
 
-    self.balance += amount
-    return self.balance
+def New_Account():
+  #function that will be called when new account button pressed
+  pass
+
+def New_Transaction(Account, id, account, creditdebit, amount, description):
+  #make this function create a new transaction object
+  if Account.id == Transaction.account:
+    Account.balance += Transaction.amount
+    print (f'{Account.name} balance is now {Account.balance}')
+  else:
+    print ('Acount ID does not match')
 
 BluevineChecking = Account('1010', 'BlueVine Checking', 'Asset', 0)
 
-print(BluevineChecking.transaction('debit', 25))
+ToolPurchase = Transaction('1234', '1010', 'credit', 90)
+
+New_Transaction(BluevineChecking, ToolPurchase)
