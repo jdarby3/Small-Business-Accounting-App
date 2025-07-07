1838a0c0ab6a5cb924e1daf3c941cc69d9741382
James D <86382991+jdarby3@users.noreply.github.com>
2025-07-07 14:21:47 -0500

    not ready yet

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from datetime import date
 
-chart_of_accounts = #should hold all accounts in a dict with the acc_id being the
-                          # key and the value being the instance
+#goal is to input coa, hournal and ledger and have them be updated
+
+chart_of_accounts = {}
+# can add logic to auto populate this dict from coa excel or csv whenever script is called
+
+transaction_tracker = {}
+# updates need to be made to the transaction function to properly populate this
 
 class Account:
   def __init__(self, acc_type, acc_id, acc_name, acc_balance=0):
@@ -10,7 +15,7 @@ class Account:
     self.acc_id = acc_id
     self.acc_name = acc_name
     self.transactions = {}
-    #add code to add each new account to my chart of accounts dict
+    chart_of_accounts[acc_id] = self
 
   def transaction(self, entry_num, d_c, amount, entity, proj_num, note, date=date.today()):
     # Define the balance change rules
@@ -25,8 +30,15 @@ class Account:
     # Record the transaction
     self.transactions[entry_num] = (d_c, amount, entity, proj_num, note, date)
 
-def journal_entry(my_csv):
-  for lines in my_csv
+def journal_entry(my_csv_file):
+  for rows in rows(my_csv)
+    if 
+    # add logic to assign debit credit variable based on if that column has data or empty
+    #this logic also assigns amount variable with amount that was in the populated column
+  ### chart_of_accounts[my_csv(column1)].transaction(my_csv(column_2), dc_variable, amount_cariable, my_csv(entity_column), etc....
+    
+    
+    
    # chart_of_accounts[acc_id].transaction(mycsv[0] ... etc
    # for every line calls .transaction on the applicable account
   pass
